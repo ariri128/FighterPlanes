0e75120352a56697bb8103ffa5611fdb433ecb3b
Arya Kamat <arya.v.kamat@gmail.com>
2025-11-10 16:44:40 -0500

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public int lives;
+    private float speed;
+
+    private GameManager gameManager;
+
+    private float horizontalInput;
+    private float verticalInput;
+
+    private float horizontalScreenLimit = 9.5f;
+
+    public GameObject bulletPrefab;
+    public GameObject explosionPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        lives = 3;
+        speed = 5.0f;
+        gameManager.ChangeLivesText(lives);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        Shooting();
+    }
+
+    public void LoseALife()
+    {
+        //lives = lives - 1;
+        //lives -= 1;
+        lives--;
+        gameManager.ChangeLivesText(lives);
+        if (lives == 0)
+        {
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
+    }
+
+    void Shooting()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bulletPrefab, transform.position + new Vector3(0, 0.5f, 0), Quaternion.identity);
+        }
+    }
+
+    void Movement()
+    {
+        //Read the input from the player
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        //Move the player
+        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * Time.deltaTime * speed);
+        //Player leaves the screen horizontally
+        if (transform.position.x > horizontalScreenLimit || transform.position.x <= -horizontalScreenLimit)
+        {
+            transform.position = new Vector3(transform.position.x * -1, transform.position.y, 0);
+        }
+        //Player leaves the screen vertically
+        if (transform.position.y > 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        if (transform.position.y <= -3.5)
+        {
+            transform.position = new Vector3(transform.position.x, -3.5f, 0);
+        }
+
+    }
+}
